94c37f7b15fced73671b6f1898f93565d67b59c1
a487czz <leochp@gmail.com>
2017-08-07 16:36:43 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { ItemFormComponent } from './items/item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent,
+    ItemDetailComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule
